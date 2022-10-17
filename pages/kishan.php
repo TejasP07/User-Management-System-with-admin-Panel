e82d1f6a724bb827383cee679a897b1e3301c592
Tejas Pawar <88932277+TejasP07@users.noreply.github.com>
2022-10-17 23:00:47 +0530

    Update kishan.php

--- pages/kishan.php
+++ pages/kishan.php
@@ -1,4 +1,5 @@
 <?php include 'include/head.php'; ?>
+<?php include 'include/templete.php'; ?>
 
 
         <div id="page-wrapper" style="background-color: lightblue;">
@@ -24,11 +25,28 @@
                             </div>
                         </div>
                         <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
+                          <div class="row">
+            <div class="col-lg-3 col-lg-offset-1">
+              <div class="form-group">
+                <label>Date Of Birth</label>
+                <input type="date" name="Date_Of_Birth" class="form-control" placeholder="Date Of Birth">
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="form-group">
+                <label>Email</label>
+                <input type="email" name="email" class="form-control" placeholder="Email">
+              </div>
+            </div>
+            <div class="col-lg-3">
+              <div class="form-group">
+                <label>Mobile Number</label>
+                <input type="tel" name="Mobile_Number" class="form-control" placeholder="Mobile Number">
+              </div>
+            </div>
+          </div>
+
+
                         </a>
                     </div>
                 </div>
