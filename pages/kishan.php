3a3a0564fc8b662b98a6adab4b1231ad288c5685
Tejas Pawar <88932277+TejasP07@users.noreply.github.com>
2022-10-17 23:08:49 +0530

    Update Kishan.php
    Added Different field , panels for enhancement for the projects

--- pages/kishan.php
+++ pages/kishan.php
@@ -1,4 +1,5 @@
 <?php include 'include/head.php'; ?>
+<?php include 'include/templete.php'; ?>
 
 
         <div id="page-wrapper" style="background-color: lightblue;">
@@ -9,7 +10,31 @@
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
-            
+            <div class="row">
+                <div class="col-lg-4 col-md-8">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i style="font-size: 20px;"><b>New User</b></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <div class="huge">00</div>
+                                    <div></div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="#">    
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
                 <div class="col-lg-4 col-md-8">
                     <div class="panel panel-green">
                         <div class="panel-heading">
@@ -23,12 +48,7 @@
                                 </div>
                             </div>
                         </div>
-                        <a href="#">
-                            <div class="panel-footer">
-                                <span class="pull-left">View Details</span>
-                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                                <div class="clearfix"></div>
-                            </div>
+                        
                         </a>
                     </div>
                 </div>
@@ -44,13 +64,7 @@
 
 
             
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <i class="fa fa-clock-o fa-fw"></i> Responsive Timeline
-                </div>
-                       
-                        <!-- /.panel-footer -->
-                </div>
+           
                     <!-- /.panel .chat-panel -->
             </div>
                 <!-- /.col-lg-4 -->
